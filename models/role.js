89286b4abec31f8c4fba06215da1833d8cd67a1b
irakli jiishkariani <irakli2692@gmail.com>
2017-05-22 13:28:31 +0400

    role model export in file

---
+++ models/role.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+
+mongoose.Promise = require('bluebird');
+
+var RoleSchema = new mongoose.Schema({
+    name: String,
+    actions: []
+});
+
+RoleSchema.methods.anotherActionHasSamePattern = function (action) {
+  for (var i = 0; i < this.actions.length; i++) {
+    var item = this.actions[i];
+
+    if(item.pattern === action.pattern && item._id !== action._id) return true;
+  }
+
+  return false;
+};
+
+RoleSchema.methods.addAction = function (action) {
+  this.actions.push(action);
+};
+
+RoleSchema.methods.updateAction = function (action) {
+
+  for (var i = 0; i < this.actions.length; i++) {
+    if (this.actions[i]._id === action._id) {
+      this.actions[i] = action;
+
+      return;
+    }
+  }
+
+};
+
+module.exports = mongoose.model('roleactions', RoleSchema);
--- umpack.js
+++ umpack.js
@@ -48,38 +48,7 @@ var User = mongoose.model('user', {
     metaData: {}
 });
 
-var RoleSchema = new mongoose.Schema({
-    name: String,
-    actions: []
-});
-
-RoleSchema.methods.anotherActionHasSamePattern = function (action) {
-  for (var i = 0; i < this.actions.length; i++) {
-    var item = this.actions[i];
-
-    if(item.pattern === action.pattern && item._id !== action._id) return true;
-  }
-
-  return false;
-};
-
-RoleSchema.methods.addAction = function (action) {
-  this.actions.push(action);
-};
-
-RoleSchema.methods.updateAction = function (action) {
-
-  for (var i = 0; i < this.actions.length; i++) {
-    if (this.actions[i]._id === action._id) {
-      this.actions[i] = action;
-
-      return;
-    }
-  }
-
-};
-
-var Role = mongoose.model('roleactions', RoleSchema);
+var Role = require('./models/role');
 
 
 router.post('/login', function(req, res, next) {
